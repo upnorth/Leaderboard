9dd762fda81defff89ae12d990544d6f2454312f
Karl Olofsson <biker.upnorth@gmail.com>
2022-10-04 11:38:52 +0200

    Initial Maven project setup with junit and .gitignore

---
+++ src/test/java/LeaderboardTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeaderboardTest {
+    @Test
+    void testSingleSuccessTest() {
+        assertEquals("test works", "test works");
+    }
+}
